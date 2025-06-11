a937c7b5c715a8136dfb7513f83b082038a8d99f
Nikita Vassiljev <vasnik@tlu.ee>
2025-06-11 15:24:51 +0300

    Update router.js

--- api/routes/ideas/v1/router.js
+++ api/routes/ideas/v1/router.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get('/', requireScopes(['auth:access', 'ideas:read', 'user:admin']), IdeasController.list);
 router.post('/', requireScopes(['auth:access', 'ideas:read', 'ideas:create']), IdeasController.create);
+router.get('/categories', requireScopes(['auth:access', 'ideas:read']), IdeasController.getCategories);
 router.get('/:id', requireScopes(['auth:access', 'ideas:read']), IdeasController.getById);
 router.patch('/:id', requireScopes(['auth:access', 'ideas:read', 'ideas:update']), IdeasController.update);
 router.delete('/:id', requireScopes(['auth:access', 'ideas:moderate']), IdeasController.delete);
