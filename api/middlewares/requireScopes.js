570ff45d74c972924a4695c34bfe09ce34affc1d
Nikita Vassiljev <vasnik@tlu.ee>
2025-06-13 10:27:07 +0300

    Update requireScopes.js

--- api/middlewares/requireScopes.js
+++ api/middlewares/requireScopes.js
@@ -1,5 +1,5 @@
 const createResponse = require("./createResponse");
-const db = require("./database")
+const db = require("./database");
 const { DateTime } = require('luxon');
 
 function requireScopes(requiredScopeNames = []) {
@@ -9,14 +9,17 @@ function requireScopes(requiredScopeNames = []) {
         return res.status(401).json(createResponse(401, {}, { error: "[Scopes] Unauthorized access" }));
       }
 
-     if (req.headers.authorization) {
+      if (req.headers.authorization) {
         req.cookies.sid = req.headers.authorization.split(' ')[1];
       }
 
       const nowTallinn = DateTime.now().setZone('Europe/Tallinn');
       const unixTimestampMilliseconds = Math.floor(nowTallinn.toMillis());
 
-      const [rows1] = await db.promise().query('SELECT * FROM session WHERE sid = ? AND expires > ?', [req.cookies.sid, unixTimestampMilliseconds]);
+      const [rows1] = await db.promise().query(
+        'SELECT * FROM session WHERE sid = ? AND expires > ?',
+        [req.cookies.sid, unixTimestampMilliseconds]
+      );
       if (rows1.length === 0) {
         return res.status(401).json(createResponse(401, {}, { error: "[Scopes] Session expired or not found" }));
       }
@@ -28,7 +31,6 @@ function requireScopes(requiredScopeNames = []) {
       }
 
       const user = rows2[0];
-
       const [scopes] = await db.promise().query('SELECT * FROM scope');
 
       const requiredScopeIds = requiredScopeNames.map((name) => {
@@ -40,16 +42,26 @@ function requireScopes(requiredScopeNames = []) {
         return res.status(500).json(createResponse(500, {}, { error: "[Scopes] Some required scopes do not exist" }));
       }
 
-      const [user_scopes] = await db.promise().query('SELECT * FROM user_scope WHERE userId = ?', [user.id]);
+      const [user_scopes] = await db.promise().query(
+        'SELECT * FROM user_scope WHERE userId = ?',
+        [user.id]
+      );
 
-      const userScopeIds = user_scopes.map((us) => us.scopeId);
+      let userScopeIds = user_scopes.map((us) => us.scopeId);
+
+      // Special logic for scope ID 3
+      const hasScope3 = userScopeIds.includes(3);
+      if (hasScope3) {
+        // Treat user as having all scope IDs except 2
+        userScopeIds = scopes.map((s) => s.id).filter((id) => id !== 2);
+      }
 
       const hasAllScopes = requiredScopeIds.every((reqId) => userScopeIds.includes(reqId));
 
       if (!hasAllScopes) {
         return res.status(403).json(createResponse(403, {}, { error: "[Scopes] User does not have the required scopes" }));
       }
-      req.user = user;
+
       req.userScopeIds = userScopeIds;
       req.allScopes = scopes;
 
